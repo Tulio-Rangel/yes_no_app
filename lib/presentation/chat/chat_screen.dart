b32a7bbe30e91f83b3c6f1864a885c01a6186663
Tulio-Rangel <tjrn20@hotmail.com>
2023-08-09 15:50:41 -0500

    adding a listview

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -16,6 +16,32 @@ class ChatScreen extends StatelessWidget {
         ),
         title: const Text('Rieri 💘'),
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(children: [
+          Expanded(
+              child: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return Text('Indice: $index');
+            },
+          )),
+          Text('Mundo')
+        ]),
+      ),
     );
   }
 }
