cf7a28938e68b8b4dd13d83a76bc55757b787561
Tulio-Rangel <tjrn20@hotmail.com>
2023-08-14 13:30:32 -0500

    create message buvle for her

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context)
+        .colorScheme; //Here we bring the color define in th e theme
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Labore est quis in eu ullamco. Mollit aute voluptate culpa do nisi labore. Duis aute laborum id et aute. Esse cupidatat ut laboris incididunt.',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+
+        _ImageBubble(),
+
+        const SizedBox(
+          height: 10,
+        )
+
+        // todo: image
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size; // Work with the device size
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/yes/1-af11222d8d4af90bdab8fc447c8cfebf.gif',
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
