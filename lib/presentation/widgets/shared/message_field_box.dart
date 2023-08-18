55bfa11a2d7c0e9f60cef135c88bcc7e1776ba5a
Tulio-Rangel <tjrn20@hotmail.com>
2023-08-18 17:55:02 -0500

    pointing to catch the message in the message field

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -9,15 +9,25 @@ class MessageFieldBox extends StatelessWidget {
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
+    final inputDecoration = InputDecoration(
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            print('Valor de la caja de texto');
+          },
+        ));
+
     return TextFormField(
-      decoration: InputDecoration(
-          enabledBorder: outlineInputBorder,
-          focusedBorder: outlineInputBorder,
-          filled: true,
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.send_outlined),
-            onPressed: () {},
-          )),
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+      },
+      onChanged: (value) {
+        print('Changed: $value');
+      },
     );
   }
 }
