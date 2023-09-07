257393b1e65dcddc16dcafb95d3d6453f169cf0a
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-06 20:59:51 -0500

    showing message using provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,16 @@
+// The folrder is called provider because we use the 'Gestor de estados' provider,
+// if the 'Gestor de estados' would be BLoC the folder would be called BloC or someting like that
+
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messageList = [
+    Message(text: 'Oliwis', fromWho: FromWho.me),
+    Message(text: 'Ya hiciste popis?', fromWho: FromWho.me)
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // TODO: implementar metodo
+  }
+}
