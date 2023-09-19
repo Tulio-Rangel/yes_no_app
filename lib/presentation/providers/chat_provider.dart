9194f483085069ed25a1c172cceaaffe16badfdf
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-19 16:58:49 -0500

    add automatic scroll when chat screen is full

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Oliwis', fromWho: FromWho.me),
     Message(text: 'Ya hiciste popis?', fromWho: FromWho.me)
@@ -14,6 +16,15 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
-    notifyListeners(); // Se notifica que hubo un cambio
+    notifyListeners(); // Se notifica que hubo un
+    moveScrollToBotton(); // Se llama a la funcion del scroll
+  }
+
+// Funcion para hacer que el chat se vaya desplazando mientras se va llenando la pantalla
+  void moveScrollToBotton() {
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
