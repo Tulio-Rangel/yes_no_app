d0de4dacb610a518565211e2dd2e57b8b8a18cc0
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-19 17:06:26 -0500

    you can't send empty messages

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -13,6 +13,8 @@ class ChatProvider extends ChangeNotifier {
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
